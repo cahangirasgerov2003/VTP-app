234e9a5098ae836d625376a8fdce012b6e3bb63f
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-08-17 23:15:36 +0400

    Last commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,164 @@
 import "./home.css";
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+import ProgressBar from "@ramonak/react-progress-bar";
+
+import { connect } from "react-redux";
+
+const Home = (props) => {
+  const Ir = props.users.filter((user) => {
+    return user.department === "İnsan Resursları";
+  });
+  const Rm = props.users.filter((user) => {
+    return user.department === "Rəqamsal Marketinq";
+  });
+  const Im = props.users.filter((user) => {
+    return user.department === "İnşaat Mühəndisliyi";
+  });
+  const Mu = props.users.filter((user) => {
+    return user.department === "Mühasibatlıq";
+  });
+  const Lo = props.users.filter((user) => {
+    return user.department === "Logistika";
+  });
+
+  const web = props.users.filter((user) => {
+    return user.department === "Web proqramlaşdırma";
+  });
+
+  const allUsers = props.users.length;
+
+  const allTable = [web, Rm, Ir, Lo, Im, Mu];
+
+  return (
+    <div>
+      <h1>İştirakçıların şöbələr üzrə paylanma faizi &#128512;</h1>
+      <div className="progressBar">
+        <p className="progressTitle">Web proqramlaşdırma : </p>
+        <ProgressBar
+          maxCompleted={100}
+          completed={((web.length / allUsers) * 100).toFixed(2)}
+          height="30px"
+          bgColor="#88157e"
+          baseBgColor="#ffacac"
+          labelAlignment="center"
+          width="50%"
+        ></ProgressBar>
+      </div>
+
+      <div className="progressBar">
+        <p className="progressTitle">İnsan Resursları : </p>
+        <ProgressBar
+          maxCompleted={100}
+          completed={((Ir.length / allUsers) * 100).toFixed(2)}
+          height="30px"
+          bgColor="#88157e"
+          baseBgColor="#ffacac"
+          labelAlignment="center"
+          width="50%"
+        ></ProgressBar>
+      </div>
+
+      <div className="progressBar">
+        <p className="progressTitle">Rəqamsal Marketinq : </p>
+        <ProgressBar
+          maxCompleted={100}
+          completed={((Rm.length / allUsers) * 100).toFixed(2)}
+          height="30px"
+          bgColor="#88157e"
+          baseBgColor="#ffacac"
+          labelAlignment="center"
+          width="50%"
+        ></ProgressBar>
+      </div>
+
+      <div className="progressBar">
+        <p className="progressTitle">İnşaat Mühəndisliyi : </p>
+        <ProgressBar
+          maxCompleted={100}
+          completed={((Im.length / allUsers) * 100).toFixed(2)}
+          height="30px"
+          bgColor="#88157e"
+          baseBgColor="#ffacac"
+          labelAlignment="center"
+          width="50%"
+        ></ProgressBar>
+      </div>
+
+      <div className="progressBar">
+        <p className="progressTitle">Mühasibatlıq : </p>
+        <ProgressBar
+          maxCompleted={100}
+          completed={((Mu.length / allUsers) * 100).toFixed(2)}
+          height="30px"
+          bgColor="#88157e"
+          baseBgColor="#ffacac"
+          labelAlignment="center"
+          width="50%"
+        ></ProgressBar>
+      </div>
+
+      <div className="progressBar">
+        <p className="progressTitle">Logistika : </p>
+        <ProgressBar
+          maxCompleted={100}
+          completed={((Lo.length / allUsers) * 100).toFixed(2)}
+          height="30px"
+          bgColor="#88157e"
+          baseBgColor="#ffacac"
+          labelAlignment="center"
+          width="50%"
+        ></ProgressBar>
+      </div>
+
+      <h1 className="tableTitle">Şöbələr üzrə iştirakçı cədvəlləri :</h1>
+      {allTable.map((user, index) => {
+        if (user.length > 0) {
+          return (
+            <div className="tableContainer" key={index}>
+              <h2 className="departmentsTitle">{`${user[0]?.department} : `}</h2>
+              <table className="table table-dark table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Surname</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Department</th>
+                    <th scope="col">University</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {user.map((user, index) => {
+                    return (
+                      <tr key={index}>
+                        <th scope="row">{index + 1}</th>
+                        <td>{user.name}</td>
+                        <td>{user.surName}</td>
+                        <td>{user.email}</td>
+                        <td>{user.age}</td>
+                        <td>{user.department}</td>
+                        <td>{user.university}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        } else {
+          return "";
+        }
+      })}
+    </div>
+  );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps)(Home);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,81 @@
+import { useNavigate } from "react-router-dom";
 import "./login.css";
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkForm = () => {
+    if (name.length >= 3 && email && password.length > 8) {
+      alert("Congratulations, you have successfully logged in");
+      navigate("/");
+    } else {
+      alert("You are required to fill in all fields !");
+    }
+    setPassword("");
+    setName("");
+    setEmail("");
+  };
+
+  return (
+    <form className="row">
+      <div className="col-12 mt-2 col-md-6">
+        <label htmlFor="name" className="form-label">
+          Name : {name}
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+        ></input>
+      </div>
+      <div className="col-12 mt-2 col-md-6">
+        <label htmlFor="email" className="form-label">
+          Email : {email}
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        ></input>
+      </div>
+      <div className="col-12 mt-2 col-md-6">
+        <label htmlFor="password" className="form-label">
+          Password : {password}
+        </label>
+        <input
+          value={password}
+          type="password"
+          className="form-control"
+          id="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div className="col-12 mt-2">
+        <button
+          type="button"
+          onClick={checkForm}
+          className="btn btn-success w-100 mt-4"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
